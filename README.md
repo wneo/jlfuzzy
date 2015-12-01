c32d4ae87f0cfcaef244faab34244562f2d1c79b
wneo <acewu@acewudeiMac.local>
2015-12-01 21:07:21 +0800

    add remove func

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
 # jlfuzzy
 fast fuzzy with Levenshtein
 
+
+search args:
+
+	// word: the word to search
+
+	// lack: max count for lack char compare to word.  (0   ~   len(word)-1)
+	//			eg. 1 for abc  -> ab/bc/ac
+	//				<0: invalid, auto to be len(word)-1;
+	//				 0: dont allow lack;
+	//				>0: allow count;
+	//
+
+	// more: max count for add char compare to word.	(int)
+	//			eg. 1 for abc  -> abcd
+	//				<0: no limit;
+	//				 0: dont allow add;
+	//				>0: allow count;
+	
+	// maxCount: max count for results.	(>0)
+
+usage:
+
     // 1. create fuzzy module (can save for cache)
     fuzzy := NewJLFuzzy()
     // 2. add words to train
--- jlfuzzy.go
+++ jlfuzzy.go
@@ -93,6 +93,19 @@ func (j *JLFuzzy) analysisWorld(word string) *WordRecord {
 	return &WordRecord{word: word, mapRuneToCount: result, TotolCount: totolCount}
 }
 
+// word: the word to search
+// lack: max count for lack char compare to word.  (0   ~   len(word)-1)
+//			eg. 1 for abc  -> ab/bc/ac
+//				<0: invalid, auto to be len(word)-1;
+//				 0: dont allow lack;
+//				>0: allow count;
+//
+// more: max count for add char compare to word.	(int)
+//			eg. 1 for abc  -> abcd
+//				<0: no limit;
+//				 0: dont allow add;
+//				>0: allow count;
+// maxCount: max count for results.	(>0)
 func (j *JLFuzzy) SearchWord(word string, lack int, more int, maxCount int) (result []string) {
 	result = []string{}
 	if len(word) == 0 {
